09dffd0cc6c3cd9b89e0919923389e8b6c3812b1
root <root@HaiAn.localdomain>
2025-07-29 08:46:10 +0000

    3:45am 29/7 an

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,23 @@
 // src/components/Navbar.jsx
 
 import React from 'react';
-import './Navbar.css'; // Kết nối với file CSS
+import { Link } from 'react-router-dom'; // <-- BƯỚC 1: IMPORT LINK
+import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar-container">
-      <div className="Logo">
-            <img src="/Logo/png"></img>
-            <div className="Hamster_English"> Hamster English</div>
-            
+      <div className="logo">
+        <img src="/logo.png" alt="Logo" className="logo-image" />
+        
+        <Link to="/" className="Hamster_English">Hamster English</Link> 
       </div>
       
       <div className="nav-links">
-        <a href="/login" className="login-button">Đăng nhập/Đăng kí</a>
+        {/* Link ngoài (đến Facebook) thì vẫn dùng thẻ <a> bình thường */}
+        <a href="https://www.facebook.com/teemo.best.750/?locale=vi_VN" target="_blank" rel="noopener noreferrer" className="nav-button contact-button">Contact Us</a>
+        <Link to="/login" className="nav-button login-button">Login/Register</Link>
+        
       </div>
     </nav>
   );
