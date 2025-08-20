4f936d6c36d013a575d12b6905470f87d6815920
root <root@HaiAn.localdomain>
2025-08-20 07:30:37 +0000

    an changed 2:30 am 20/8

--- english-write-ai/src/components/Navbar.jsx
+++ english-write-ai/src/components/Navbar.jsx
@@ -1,19 +1,25 @@
-// src/components/Navbar.jsx
+
 
 import React from 'react';
-// <<<<<<< HEAD
-// import '../index.css'; // Kết nối với file CSS
-// =======
-import { Link } from 'react-router-dom'; // <-- BƯỚC 1: IMPORT LINK
+
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext.jsx'
 import './Navbar.css';
 
-// >>>>>>> 09dffd0cc6c3cd9b89e0919923389e8b6c3812b1
+
 
 function Navbar() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  // Hàm xử lý đăng xuất
+  const handleLogout = () => {
+    logout();
+    navigate('/login'); // Chuyển hướng về trang đăng nhập sau khi đăng xuất
+  };
   return (
     <nav className="navbar-container">
       <div className="logo">
-        <img src="/assets/logo.png" alt="Logo" className="logo-image" />
         
         <Link to="/" className="Hamster_English">Hamster English</Link> 
       </div>
@@ -21,7 +27,15 @@ function Navbar() {
       <div className="nav-links">
         {/* Link ngoài (đến Facebook) thì vẫn dùng thẻ <a> bình thường */}
         <a href="https://www.facebook.com/teemo.best.750/?locale=vi_VN" target="_blank" rel="noopener noreferrer" className="nav-button contact-button">Contact Us</a>
-        <Link to="/login" className="nav-button login-button">Login/Register</Link>
+        { user ? (
+          <>
+          <a href="#" onClick={handleLogout} className="navbar-link">Logout</a>
+          <Link to="/profile" className="navbar-profile-icon"><i className="fas fa-user-circle"></i></Link>
+
+          </>
+        ) : (
+          <Link to="/login" className="nav-button login-button">Login/Register</Link>
+        )}
         
       </div>
     </nav>
